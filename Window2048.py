df00674899d6d83cd33fddc19ed4144e8772814c
Owhenthesaints <owen.simon@epfl.ch>
2024-02-02 12:57:34 +0100

    added new Qt clas BackgroundGrid+ started Box2048View

--- Window2048.py
+++ Window2048.py
@@ -1,23 +1,16 @@
-from PyQt6.QtWidgets import QWidget, QGridLayout, QPushButton
+from typing import List, Tuple
+from PyQt6.QtWidgets import QMainWindow, QWidget, QGraphicsScene, QGraphicsView, QVBoxLayout, QGraphicsRectItem, QGraphicsTextItem
 from PyQt6.QtGui import QPainter, QColor
+import numpy as np
+
+DEFAULT_WINDOW_SIZE = (300, 300, 300, 300)
 
 
-class Window2048(QWidget):
-    def __init__(self, size: tuple = (4, 4), window_size: tuple = (300, 300, 300, 300)) -> None:
-        """
-        A class to instantiate a Window which can host a 2048 game.
-        :param window_size: x, y and initial width and height of the window
-        :type window_size: tuple
-        :param size: n then m representing size of playable area
-        :return: None
-        :rtype: None
-        :type size: tuple
-        """
-        super().__init__()
+class BackgroundGrid(QWidget):
+
+    def __init__(self, size: tuple = (4, 4), parent=None):
+        super().__init__(parent)
         self._SIZE = size
-        self.setGeometry(*window_size)
-        self.setWindowTitle('2048 Game')
-        self.setStyleSheet('background-color:grey; color: black')
 
     def paintEvent(self, a0):
         painter = QPainter(self)
@@ -35,3 +28,39 @@ class Window2048(QWidget):
     def resizeEvent(self, event):
         super().resizeEvent(event)
         self.update()
+
+
+class Box2048(QGraphicsView):
+    def __init__(self, board: np.ndarray = None, parent=None):
+        super().__init__(parent)
+        self.scene = QGraphicsScene(self)
+        self.setScene(self.scene)
+        # set the boxes
+        self.black_boxes: List[List[QGraphicsRectItem, QGraphicsTextItem]] = []
+        black_box = QGraphicsRectItem(0, 0, 100, 100)
+        black_box.setBrush(QColor(0, 0, 0))
+
+
+class Window2048(QMainWindow):
+    def __init__(self, size: tuple = (4, 4), window_size: tuple = DEFAULT_WINDOW_SIZE) -> None:
+        """
+        A class to instantiate a Window which can host a 2048 game.
+        :param window_size: x, y and initial width and height of the window
+        :type window_size: tuple
+        :param size: n then m representing size of playable area
+        :return: None
+        :rtype: None
+        :type size: tuple
+        """
+        super().__init__()
+        self.setWindowTitle("2048 Game")
+        self.setGeometry(*window_size)
+
+        # setting up different widgets
+        self.centralWidget = QWidget(self)
+        self.setCentralWidget(self.centralWidget)
+        self.background_widget = BackgroundGrid(size, self.centralWidget)
+        self.layout = QVBoxLayout(self.centralWidget)
+        self.layout.addWidget(self.background_widget)
+        self.setWindowTitle('2048 Game')
+        self.setStyleSheet('background-color:grey; color: black')
